56432ffd5fefb6553bf11455f0e1088e08a51308
abdullahalnoor007 <aalnoor74@gmail.com>
2021-09-15 12:31:41 +0600

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+            <link rel="stylesheet" href="{{asset('admin/css/bootstrap.min.css')}}">
+            <link rel="stylesheet" href="{{asset('admin/css/datatables.min.css')}}">
+            <link rel="stylesheet" href="{{asset('admin/css/responsive.bootstrap4.min.css')}}">
+            <link rel="stylesheet" href="{{asset('admin/css/responsive.dataTables.min.css')}}">
+     
+    </head>
+    <body class="antialiased">
+      
+
+
+
+
+
+
+
+
+
+<script src="{{asset('admin/js/jquery-3.6.0.min.js')}}"></script>
+<script src="{{asset('admin/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('admin/js/datatables.min.js')}}"></script>
+<script src="{{asset('admin/js/dataTables.bootstrap4.min.js')}}"></script>
+<script src="{{asset('admin/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{asset('admin/js/responsive.bootstrap4.min.js')}}"></script>
+
+    </body>
+</html>
